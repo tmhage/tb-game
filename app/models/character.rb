5d07a00a52dbbc9659ec2faa04c72ba54e17f32d
tmhage <attmhage@gmail.com>
2016-10-08 19:23:40 +0200

    add single active char validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,12 @@
 class Character < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validate :have_active_character?
   validates_presence_of :name,:image,:gender,:user
   validates_uniqueness_of :name, :message => "This name is already being used"
+
+  def have_active_character?
+    !self.user.active_character
+    errors.add(:base, "You already have an active character")
+  end
 end
