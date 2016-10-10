f2a2c5eac97856a6999787953c3af8371f105ba0
tmhage <attmhage@gmail.com>
2016-10-10 16:41:15 +0200

    add working online list

--- config/initializers/set_offline.rb
+++ config/initializers/set_offline.rb
@@ -1,6 +1,5 @@
 Warden::Manager.before_logout do |user, auth, opts|
-  char = user.active_character
   if user.active_character
-  char.update(online:false)
+    user.active_character.toggle!(:online)
   end
 end
